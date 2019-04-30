fe697365f4673819200e0532f4e4784d7a976b79
SalimovaNellia <salimova.nellia@gmail.com>
2019-04-30 16:50:02 +0300

    store-model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div class="bg-success p-a-1 text-xs-center">
+                  This is SportsStore
+              </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
