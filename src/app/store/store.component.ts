630e0d5177969be3f143c4deb5bcc5f09b75c14e
SalimovaNellia <salimova.nellia@gmail.com>
2019-05-02 16:59:23 +0300

    Creating a non-standard directive

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -36,9 +36,16 @@ export class StoreComponent {
         this.changePage(1);
     }
 
-    get pageNumbers(): number[] {
-        return Array(Math.ceil(this.repository
-            .getProducts(this.selectedCategory).length / this.productsPerPage))
-            .fill(0).map((x, i) => i + 1);
+    get pageCount(): number {
+        return Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage)
     }
+
+    // get pageNumbers(): number[] {
+    //     return Array(Math.ceil(this.repository
+    //         .getProducts(this.selectedCategory).length / this.productsPerPage))
+    //         .fill(0).map((x, i) => i + 1);
+    // }
+
+     
 }
